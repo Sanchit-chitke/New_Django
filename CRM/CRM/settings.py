54586750045a74dc2805fc6cc95954d934143da5
Sanchit-chitke <sanchitchitke532@gmail.com>
2025-03-27 13:58:04 +0530

    Database setup

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crmdb1',
+        'USER': 'root',
+        'PASSWORD': 'Sanchit@5320',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
